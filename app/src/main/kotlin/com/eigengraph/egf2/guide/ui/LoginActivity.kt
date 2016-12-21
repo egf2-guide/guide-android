bbebb73c528b228e6c079a362bf4a16b316d6f07
alexxxdev <alexxxdev@yandex.ru>
2016-12-21 10:55:17 +0700

    fix close LoginActivity after logging

--- app/src/main/kotlin/com/eigengraph/egf2/guide/ui/LoginActivity.kt
+++ app/src/main/kotlin/com/eigengraph/egf2/guide/ui/LoginActivity.kt
@@ -18,7 +18,7 @@ class LoginActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		if (EGF2.isLoggedIn()) {
-			finish()
+			finishAffinity()
 			this.startActivity<MainActivity>()
 		} else {
 			setContentView(loginLayout.bind(this))
